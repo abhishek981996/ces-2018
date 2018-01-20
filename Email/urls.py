bd3b30421ba6c58ad6c8edd015d0043e5d3019d4
abhishek981996 <abhishektiwari98199@gmail.com>
2018-01-20 19:06:16 +0530

    changes done

--- Email/urls.py
+++ Email/urls.py
@@ -1,8 +1,8 @@
 
-# from django.conf.urls import url,include
-# from django.contrib import admin
-# from Email import views
-# urlpatterns = [
-#     url(r'^sendmail/$',views.SendMail, name="SendMail"),
+from django.conf.urls import url,include
+from django.contrib import admin
+from Email import views
+urlpatterns = [
+    url(r'^sendmail/$',views.SendMail, name="SendMail"),
 
-# ]
+]
