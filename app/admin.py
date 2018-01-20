f6da5cf85b36a08827e104a3ff1dc1b2ffb8447e
abhishek981996 <abhishektiwari98199@gmail.com>
2018-01-20 18:57:11 +0530

    changes done

--- app/admin.py
+++ app/admin.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# # -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
 
-from django.contrib import admin
-from app.models import Event,UserName,Email,PhoneNo,AdmissionNo,Branch,TeamName,Gender,Idea,Registration
-
-
-admin.site.register(Event)
-admin.site.register(UserName)
-admin.site.register(Email)
-admin.site.register(PhoneNo)
-admin.site.register(AdmissionNo)
-admin.site.register(Branch)
-admin.site.register(TeamName)
-admin.site.register(Gender)
-admin.site.register(Idea)
-admin.site.register(Registration)
+# from django.contrib import admin
+# from app.models import Event,UserName,Email,PhoneNo,AdmissionNo,Branch,TeamName,Gender,Idea,Registration
 
 
-# Register your models here.
+# admin.site.register(Event)
+# admin.site.register(UserName)
+# admin.site.register(Email)
+# admin.site.register(PhoneNo)
+# admin.site.register(AdmissionNo)
+# admin.site.register(Branch)
+# admin.site.register(TeamName)
+# admin.site.register(Gender)
+# admin.site.register(Idea)
+# admin.site.register(Registration)
+
+
+# # Register your models here.
